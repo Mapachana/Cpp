17eee97bf309c36c792ab67ad94e3c3a1e2747ea
Miguel Lentisco Ballesteros <miguel_lb_97@hotmail.com>
2018-10-18 17:11:55 +0200

    Create lasuma.cpp

---
+++ lasuma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int a, b;
+  cout << "Dime un numero cari" << endl;
+  cin >> a;
+  cout << "dame otro" << endl;
+  cin >> b;
+  cout << "La suma es: " << a + b << endl;
+
+}
