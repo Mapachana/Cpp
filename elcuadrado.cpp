001d9d58e00654f1730844364c5e5ef559061189
Miguel Lentisco Ballesteros <miguel_lb_97@hotmail.com>
2018-10-18 17:10:19 +0200

    Create elcuadrado.cpp

---
+++ elcuadrado.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  int c;
+
+  cout << "Dime un numero del 1 al 10" << endl;
+  cin >> c;
+  cout << "El cuadrado es: " << c*c << endl;
+
+}
